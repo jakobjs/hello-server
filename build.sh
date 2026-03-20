d66f3abf2ab3e87c852f1de09d0b35449ec66ae4
jakob <jakobjs@gmail.com>
2026-03-20 23:46:09 +0000

    feat: multi-arch docker build

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-docker build . -t jakobjs/hello-server:v0.0.1
-docker push jakobjs/hello-server:v0.0.1
+docker buildx build --platform linux/amd64,linux/arm64 --push -t jakobjs/hello-server:v0.0.1 .
